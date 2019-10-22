3a66407a81d7f751b825a2458534e52ead723f32
JoshSample <joshsample98@gmail.com>
2019-10-22 13:36:01 -0500

    Part 1 finished

--- CSCI3370/basics.pl
+++ CSCI3370/basics.pl
@@ -1,6 +1,28 @@
 # Josh Sample
 # Lab 2
 # CSCI 3370
+use strict;
+use warnings;
 
-print "What is your name? ";
+# Get first name
+print "What is your first name? ";
 my $fname = readline STDIN;
+
+# Get last name
+print "What is your last name? ";
+my $lname = readline STDIN;
+
+# Get home town
+print "Where is your home town? ";
+my $town = readline STDIN;
+
+# Get temperature
+print "What's the current temperature? ";
+my $temp = readline STDIN;
+
+# Print inputs
+chomp ($fname);
+print "\nYour first name: ", $fname, "\n";
+print "Your last name: ", $lname;
+print "Your home town: ", $town;
+print "The temperature: ", $temp;
